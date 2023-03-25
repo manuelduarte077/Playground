81b25733e450c912b389cf1a78177e3a9fe835c8
Manuel Duarte  <carlosd99m@gmail.com>
2023-03-24 20:12:43 -0600

    basic examples

---
+++ cpp/datatypes.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+  int integer = 20;        // Type: integer
+  float floatante = 20.55; // Type: float
+  double doublee = 20.556; // Type: double
+  char letra = 'a';
+
+  cout << "Integer: " << integer << endl;
+  cout << "Float: " << floatante << endl;
+  cout << "Double: " << doublee << endl;
+  cout << "Char: " << letra << endl;
+
+  return 0;
+}
