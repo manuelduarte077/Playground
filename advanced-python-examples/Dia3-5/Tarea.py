1c712415d18f0f3e71afb1e55285626963d9bb8f
Manuel Duarte <carlosd99m@gmail.com>
2024-10-26 22:37:54 -0600

    ADD: Implement basic math operations and string manipulation

---
+++ advanced-python-examples/Dia3-5/Tarea.py
@@ -0,0 +1,27 @@
+import random
+
+def adivina_el_numero():
+    # Generar un número aleatorio entre 1 y 100
+    numero_a_adivinar = random.randint(1, 100)
+    intentos = 0
+
+    print("¡Bienvenido al juego de adivinar el número!")
+    print("Estoy pensando en un número entre 1 y 100.")
+    print("Intenta adivinarlo.")
+
+    while True:
+        # Pedir al usuario que ingrese un número
+        intento = int(input("Introduce tu intento: "))
+        intentos += 1  # Aumentar el contador de intentos
+
+        # Comparar el intento con el número a adivinar
+        if intento < numero_a_adivinar:
+            print("Demasiado bajo. Intenta de nuevo.")
+        elif intento > numero_a_adivinar:
+            print("Demasiado alto. Intenta de nuevo.")
+        else:
+            print(f"¡Felicidades! Adivinaste el número en {intentos} intentos.")
+            break
+
+# Ejecutar el juego
+adivina_el_numero()
---
+++ advanced-python-examples/Dia3-5/elseif.py
@@ -0,0 +1,46 @@
+edad = 20
+
+if edad < 18:
+    print("Eres menor de edad.")
+elif edad < 65:
+    print("Eres adulto.")
+else:
+    print("Eres un adulto mayor.")
+
+
+
+# Bucle for que imprime cada número del 1 al 5
+for i in range(1, 6):
+    print("Número:", i)
+
+# Bucle while que cuenta hacia atrás desde 5
+contador = 5
+while contador > 0:
+    print("Contador:", contador)
+    contador -= 1
+
+# Función que suma dos números y devuelve el resultado
+def nombre_funcion(parametro1, parametro2):
+    resultado = parametro1 + parametro2
+
+    return resultado
+
+# Hola, Ana 
+def saludo(nombre):
+    mensaje = "Hola, " + nombre
+    return mensaje
+
+print(saludo("Ana"))
+
+
+# Variable global
+mensaje_global = "Hola desde fuera de la función"
+
+def funcion():
+    # Variable local
+    mensaje_local = "Hola desde dentro de la función"
+    print(mensaje_local)
+
+funcion()
+print(mensaje_global)
+# print(mensaje_local)  # Esto generaría un error porque mensaje_local no es accesible aquí
