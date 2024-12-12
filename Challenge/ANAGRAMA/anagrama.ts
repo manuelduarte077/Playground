28096f949ba48c14317ef38b5ef4bba78192b228
Manuel Duarte <carlosd99m@gmail.com>
2024-12-11 22:59:06 -0600

    refactor code

--- Challenge/ANAGRAMA/anagrama.ts
+++
@@ -1,8 +0,0 @@
-const sonAnagramas = (palabra1: string, palabra2: string): boolean => {
-  const palabra1Ordenada = palabra1.split("").sort().join("");
-  const palabra2Ordenada = palabra2.split("").sort().join("");
-
-  return palabra1Ordenada === palabra2Ordenada;
-};
-
-console.log(sonAnagramas("amor", "roma"));
